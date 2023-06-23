49d80d837192465dcbc8b188d5ad7cc436e28314
ajieOwO <ajie.wang@qq.com>
2023-06-23 19:40:29 +0800

    doc：调整d.ts类型声明

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,3 @@
-import AjieLog from "./src";
-
 declare module "ajielog"{
 	export default AjieLog;
 	export {
@@ -7,5 +5,64 @@ declare module "ajielog"{
 	}
 }
 
+declare class AjieLog {
+	constructor(config: {
+		name: string | undefined,
+		file: {
+			root_path: string | undefined,
+			split_by_date: boolean | undefined,
+			extension: string | undefined,
+			file_name: string | undefined
+		} | undefined,
+		format: {
+			force_single_row: boolean
+		},
+		level: {
+			file: number,
+			console: number
+		} | number | undefined,
+		time_format: string | undefined,
+		save_interval: number,
+	} | undefined);
 
-export default AjieLog;
+	/**
+	 * 获取一个实例
+	 */
+	static getInstance(name: string): AjieLog;
+
+	/**
+	 * 指定类型写入一行日志
+	 */
+	log(content: string, type: ?string): void;
+
+	/**
+	 * 写入一行debug类型日志
+	 */
+	debug(content: string): void;
+
+	/**
+	 * 写入一行debug类型日志
+	 */
+	info(content: string): void;
+
+	/**
+	 * 写入一行warn类型日志
+	 */
+	warn(content: string): void;
+
+	/**
+	 * 写入一行err类型日志
+	 */
+	err(content: string): void;
+
+	/**
+	 * 写入一行fatal类型日志
+	 */
+	fatal(content: string): void;
+
+	/**
+	 * 手动保存日志
+	 */
+	saveLog(): Promise<void>
+}
+
