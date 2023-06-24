5dd0ee007cd8e5aed3fdf100444128b1049e7133
ajieOwO <ajie.wang@qq.com>
2023-06-24 15:44:14 +0800

    doc：新增readme文档

--- test/index.js
+++ test/index.js
@@ -1,7 +1,22 @@
 import AjieLog from "ajielog";
 
 let t = new AjieLog({
-	save_interval: 0
+	name: "日志1",	// 日志模块名称
+	file: {
+		root_path: "my_log_dir", // 日志文件保存根路径
+		split_by_date: true,	// 日志是否按日期分割
+		extension: "",	// 日志文件拓展名
+		file_name: ""	// 日志文件前缀
+	},
+	format: {
+		force_single_row: true	// 日志是否强制单行
+	},
+	level: {
+		file: 2,	// 文件保存的日志等级
+		console: 0	// 控制台输出的日志等级
+	},
+	time_format: "YYYY-MM-DD HH:mm:ss.SSS",	// 时间日期的格式
+	save_interval: 0	// 日志文件保存间隔（0为立即保存）
 });
 
 t.debug("debug content");
